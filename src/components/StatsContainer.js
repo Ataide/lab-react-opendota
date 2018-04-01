add3eeb9787ec6ed5c46e8ddf20e7776f9e7bf34
Ataide Bastos <ataide.bastos@gmail.com>
2018-04-01 17:15:36 -0300

    Semantic-UI Added

--- src/components/StatsContainer.js
+++ src/components/StatsContainer.js
@@ -18,56 +18,54 @@ class StatsContainer extends Component {
     componentDidMount() {
         fetchStats().then(function(response) {
             console.log(response.data);
-        });  
-
-
+        });
     }
 
     render() { 
         return( 
 
-        <div class="box">
+        <div className="box">
           
-            <div class="box__header">
-                <h3 class="box__header-title">Noryas</h3>
-                <img class="box__header-img" src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b6/b6fb4ea763781ef30a48cf025dcf926eeec0de38_medium.jpg"></img>
+            <div className="box__header">
+                <h3 className="box__header-title">Noryas</h3>
+                <img className="box__header-img" src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b6/b6fb4ea763781ef30a48cf025dcf926eeec0de38_medium.jpg"></img>
             </div>
 
-            <div class="box__body">
+            <div className="box__body">
                 
-                <div class="stats stats--main">
-                    <div class="stats__amount">12698</div>
-                    <div class="stats__caption">WINS</div>
-                    <div class="stats__change">
-                    <div class="stats__value stats__value--positive">+6%</div>
-                    <div class="stats__period">this week</div>
+                <div className="stats stats--main">
+                    <div className="stats__amount">12698</div>
+                    <div className="stats__caption">WINS</div>
+                    <div className="stats__change">
+                    <div className="stats__value stats__value--positive">+6%</div>
+                    <div className="stats__period">this week</div>
                     </div>
                 </div>
         
-                <div class="stats">
-                    <div class="stats__amount">87359</div>
-                    <div class="stats__caption">LOSSES</div>
-                    <div class="stats__change">
-                    <div class="stats__value stats__value--negative">-12%</div>
-                    <div class="stats__period">this week</div>
+                <div className="stats">
+                    <div className="stats__amount">87359</div>
+                    <div className="stats__caption">LOSSES</div>
+                    <div className="stats__change">
+                    <div className="stats__value stats__value--negative">-12%</div>
+                    <div className="stats__period">this week</div>
                     </div>
                 </div>
         
-                <div class="stats">
-                    <div class="stats__amount">6302</div>
-                    <div class="stats__caption">LOSSES</div>
-                    <div class="stats__change">
-                    <div class="stats__value">+78</div>
-                    <div class="stats__period">this week</div>
+                <div className="stats">
+                    <div className="stats__amount">6302</div>
+                    <div className="stats__caption">LOSSES</div>
+                    <div className="stats__change">
+                    <div className="stats__value">+78</div>
+                    <div className="stats__period">this week</div>
                     </div>
                 </div>
         
-                <div class="stats">
-                    <div class="stats__amount">268</div>
-                    <div class="stats__caption">posts</div>
-                    <div class="stats__change">
-                    <div class="stats__value">+3</div>
-                    <div class="stats__period">this week</div>
+                <div className="stats">
+                    <div className="stats__amount">268</div>
+                    <div className="stats__caption">posts</div>
+                    <div className="stats__change">
+                    <div className="stats__value">+3</div>
+                    <div className="stats__period">this week</div>
                     </div>
                 </div>
             </div>
